0661ef23f8dfacb9c62a487b024c1c6abb79484a
fjp <fjp@e2a10ce1-105c-4f14-853e-4fe7a960f486>
2006-03-08 18:18:34 +0000

    Preparando la creación de un tema desde cero.
    git-svn-id: https://svn.forge.osor.eu/svn/gvsig-desktop/trunk/extensions/extCAD@4328 e2a10ce1-105c-4f14-853e-4fe7a960f486

---
+++ src/com/iver/cit/gvsig/NewTheme.java
@@ -0,0 +1,102 @@
+package com.iver.cit.gvsig;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+import jwizardcomponent.Utilities;
+import jwizardcomponent.example.SimpleDynamicWizardPanel;
+import jwizardcomponent.example.SimpleLabelWizardPanel;
+import jwizardcomponent.frame.SimpleLogoJWizardFrame;
+
+import com.iver.andami.PluginServices;
+import com.iver.andami.plugins.Extension;
+import com.iver.cit.gvsig.gui.View;
+
+/**
+ * DOCUMENT ME!
+ * 
+ * @author Vicente Caballero Navarro
+ */
+public class NewTheme implements Extension {
+	static ImageIcon LOGO;
+
+	/**
+	 * @see com.iver.andami.plugins.Extension#inicializar()
+	 */
+	public void inicializar() {
+	}
+
+	/**
+	 * @see com.iver.andami.plugins.Extension#execute(java.lang.String)
+	 */
+	public void execute(String actionCommand) {
+		com.iver.andami.ui.mdiManager.View f = PluginServices.getMDIManager()
+				.getActiveView();
+
+		if (f instanceof View) {
+			View vista = (View) f;
+
+			LOGO = new javax.swing.ImageIcon("images/package_graphics.png");
+			// new
+			// ImageIcon(DefaultJWizardComponents.class.getResource("images/logo.jpeg"));
+
+			SimpleLogoJWizardFrame wizardFrame = new SimpleLogoJWizardFrame(
+					LOGO);
+			wizardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			SwingUtilities.updateComponentTreeUI(wizardFrame);
+
+			wizardFrame.setTitle("Creación de un nuevo Tema");
+
+			wizardFrame.getWizardComponents()
+					.addWizardPanel(
+							new SimpleLabelWizardPanel(wizardFrame
+									.getWizardComponents(), new JLabel(
+									"Dynamic Test")));
+
+			wizardFrame.getWizardComponents().addWizardPanel(
+					new SimpleDynamicWizardPanel(wizardFrame
+							.getWizardComponents()));
+
+			wizardFrame.getWizardComponents().addWizardPanel(
+					new SimpleLabelWizardPanel(wizardFrame
+							.getWizardComponents(), new JLabel("Done!")));
+			wizardFrame.setSize(500, 300);
+			Utilities.centerComponentOnScreen(wizardFrame);
+			wizardFrame.show();
+		}
+	}
+
+	/**
+	 * @see com.iver.andami.plugins.Extension#isEnabled()
+	 */
+	public boolean isEnabled() {
+		View f = (View) PluginServices.getMDIManager().getActiveView();
+
+		if (f == null) {
+			return false;
+		} else
+			return true;
+	}
+
+	/**
+	 * @see com.iver.andami.plugins.Extension#isVisible()
+	 */
+	public boolean isVisible() {
+		com.iver.andami.ui.mdiManager.View f = PluginServices.getMDIManager()
+				.getActiveView();
+
+		if (f == null) {
+			return false;
+		}
+
+		if (f.getClass() == View.class) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+}
