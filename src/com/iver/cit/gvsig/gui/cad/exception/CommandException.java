0e398dd29482dded029f6d327ef89f9c3be5e15c
caballero <caballero@e2a10ce1-105c-4f14-853e-4fe7a960f486>
2006-06-08 08:02:15 +0000

    cambio de nombre
    git-svn-id: https://svn.forge.osor.eu/svn/gvsig-desktop/trunk/extensions/extCAD@5734 e2a10ce1-105c-4f14-853e-4fe7a960f486

--- src/com/iver/cit/gvsig/gui/cad/exception/CommandException.java
+++ src/com/iver/cit/gvsig/gui/cad/exception/CommandException.java
@@ -1,23 +1,23 @@
 package com.iver.cit.gvsig.gui.cad.exception;
 
-public class CommadException extends Exception {
+public class CommandException extends Exception {
 
-	public CommadException() {
+	public CommandException() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public CommadException(String arg0) {
+	public CommandException(String arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
 
-	public CommadException(Throwable arg0) {
+	public CommandException(Throwable arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
 
-	public CommadException(String arg0, Throwable arg1) {
+	public CommandException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
 	}
