a20900c48f5b6ae07dfc23fd500cce9d33c9f4d4
Pablo Sanxiao <psanxiao@cartolab.es>
2011-08-18 18:44:17 +0200

    Merge of OpenCADTools
    Added RedigitalizePolygon Tool

---
+++ src/com/iver/cit/gvsig/RedigitalizePolygonExtension.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2008 Deputación Provincial de A Coruña
+ * Copyright 2009 Deputación Provincial de Pontevedra
+ * Copyright 2010 CartoLab, Universidad de A Coruña
+ *
+ * This file is part of openCADTools, developed by the Cartography
+ * Engineering Laboratory of the University of A Coruña (CartoLab).
+ * http://www.cartolab.es
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,USA.
+ */
+package com.iver.cit.gvsig;
+
+import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
+import com.iver.andami.PluginServices;
+import com.iver.andami.messages.NotificationManager;
+import com.iver.andami.plugins.Extension;
+import com.iver.cit.gvsig.fmap.MapControl;
+import com.iver.cit.gvsig.fmap.layers.FLyrVect;
+import com.iver.cit.gvsig.project.documents.view.gui.View;
+import com.iver.cit.gvsig.gui.cad.tools.RedigitalizePolygonCADTool;
+
+
+/**
+ * Extension to redigitalize polygons on edition
+ *
+ * @author Jose Ignacio Lamas [LBD]
+ * @author Nacho Varela [Cartolab]
+ * @author Pablo Sanxiao [CartoLab]
+ */
+public class RedigitalizePolygonExtension extends Extension {
+	private final String iconPath = "images/RedigitalizePolygon.png";
+	private final String iconCode = "edition-geometry-redigitalize-polygon";
+	private final String cadToolCode = "_redigitalize_polygon";
+	private View view;
+
+	private MapControl mapControl;
+	private RedigitalizePolygonCADTool polygon;
+
+	/**
+	 * @see com.iver.andami.plugins.IExtension#initialize()
+	 */
+	public void initialize() {
+		polygon = new RedigitalizePolygonCADTool();
+		CADExtension.addCADTool(cadToolCode, polygon);
+		registerIcon();
+	}
+
+	/**
+	 * @see com.iver.andami.plugins.IExtension#execute(java.lang.String)
+	 */
+	public void execute(String s) {
+		CADExtension.initFocus();
+		if (s.equals(cadToolCode)) {
+			CADExtension.setCADTool(cadToolCode,true);
+			CADExtension.getEditionManager().setMapControl(mapControl);
+		}
+		CADExtension.getCADToolAdapter().configureMenu();
+	}
+
+	/**
+	 * @see com.iver.andami.plugins.IExtension#isEnabled()
+	 */
+	public boolean isEnabled() {
+
+		if (EditionUtilities.getEditionStatus() == EditionUtilities.EDITION_STATUS_ONE_VECTORIAL_LAYER_ACTIVE_AND_EDITABLE) {
+			view = (View) PluginServices.getMDIManager().getActiveWindow();
+			mapControl = view.getMapControl();
+			if (CADExtension.getEditionManager().getActiveLayerEdited()==null)
+				return false;
+			FLyrVect lv=(FLyrVect)CADExtension.getEditionManager().getActiveLayerEdited().getLayer();
+
+				try {
+					if (polygon.isApplicable(lv.getShapeType()))
+						return true;
+				} catch (ReadDriverException e) {
+					NotificationManager.addError(e.getMessage(), e);
+				}
+
+//			LayerDescriptor ld = LayerManager.getLayerDescriptor(lv.getName());
+//			String tipoGeom = ld.getLayerEditionDescriptor().getTipoGeom();
+//			if (poligono.newIsApplicable(ld)){
+//				return true;
+//			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @see com.iver.andami.plugins.IExtension#isVisible()
+	 */
+	public boolean isVisible() {
+		if (EditionUtilities.getEditionStatus() == EditionUtilities.EDITION_STATUS_ONE_VECTORIAL_LAYER_ACTIVE_AND_EDITABLE)
+			return true;
+		return false;
+	}
+	
+	private void registerIcon(){
+		PluginServices.getIconTheme().registerDefault(
+				iconCode,
+				this.getClass().getClassLoader().getResource(iconPath)
+			);
+	}
+}
---
+++ src/com/iver/cit/gvsig/gui/cad/tools/smc/RedigitalizePolygonCADToolContext.java
@@ -0,0 +1,567 @@
+/*
+ * Copyright 2008 Deputación Provincial de A Coruña
+ * Copyright 2009 Deputación Provincial de Pontevedra
+ * Copyright 2010 CartoLab, Universidad de A Coruña
+ *
+ * This file is part of openCADTools, developed by the Cartography
+ * Engineering Laboratory of the University of A Coruña (CartoLab).
+ * http://www.cartolab.es
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,USA.
+ */
+
+
+package com.iver.cit.gvsig.gui.cad.tools.smc;
+
+import com.iver.cit.gvsig.gui.cad.tools.RedigitalizePolygonCADTool;
+
+import java.awt.event.InputEvent;
+import com.iver.andami.PluginServices;
+
+/**
+ * @author Jose Ignacio Lamas Fonte [LBD]
+ * @author Nacho Varela [Cartolab]
+ * @author Pablo Sanxiao [CartoLab]
+ */
+public final class RedigitalizePolygonCADToolContext
+extends statemap.FSMContext
+{
+//	---------------------------------------------------------------
+//	Member methods.
+
+
+	public RedigitalizePolygonCADToolContext(RedigitalizePolygonCADTool owner)
+	{
+		super();
+
+		_owner = owner;
+		setState(RedigitalizePolygon.FirstPoint);
+		RedigitalizePolygon.FirstPoint.Entry(this);
+	}
+
+	public void addOption(String s)
+	{
+		_transition = "addOption";
+		getState().addOption(this, s);
+		_transition = "";
+		return;
+	}
+
+	public void addPoint(double pointX, double pointY, InputEvent event)
+	{
+		_transition = "addPoint";
+		getState().addPoint(this, pointX, pointY, event);
+		_transition = "";
+		return;
+	}
+
+	public void removePoint(InputEvent event, int numPoints)
+	{
+		_transition = "removePoint";
+		getState().removePoint(this, event, numPoints);
+		_transition = "";
+		return;
+	}
+
+	public RedigitalizePolygonCADToolState getState()
+	throws statemap.StateUndefinedException
+	{
+		if (_state == null)
+		{
+			throw(
+					new statemap.StateUndefinedException());
+		}
+
+		return ((RedigitalizePolygonCADToolState) _state);
+	}
+
+	protected RedigitalizePolygonCADTool getOwner()
+	{
+		return (_owner);
+	}
+
+//	---------------------------------------------------------------
+//	Member data.
+
+
+	transient private RedigitalizePolygonCADTool _owner;
+
+//	---------------------------------------------------------------
+//	Inner classes.
+
+
+	public static abstract class RedigitalizePolygonCADToolState
+	extends statemap.State
+	{
+		//-----------------------------------------------------------
+		// Member methods.
+		//
+
+		protected RedigitalizePolygonCADToolState(String name, int id)
+		{
+			super (name, id);
+		}
+
+		protected void Entry(RedigitalizePolygonCADToolContext context) {}
+		protected void Exit(RedigitalizePolygonCADToolContext context) {}
+
+		protected void addOption(RedigitalizePolygonCADToolContext context, String s)
+		{
+			Default(context);
+		}
+
+		protected void addPoint(RedigitalizePolygonCADToolContext context, double pointX, double pointY, InputEvent event)
+		{
+			Default(context);
+		}
+
+		protected void removePoint(RedigitalizePolygonCADToolContext context, InputEvent event, int numPoints)
+		{
+			Default(context);
+		}
+
+		protected void Default(RedigitalizePolygonCADToolContext context)
+		{
+			throw (
+					new statemap.TransitionUndefinedException(
+							"State: " +
+							context.getState().getName() +
+							", Transition: " +
+							context.getTransition()));
+		}
+
+		//-----------------------------------------------------------
+		// Member data.
+		//
+	}
+
+	/* package */ static abstract class RedigitalizePolygon
+	{
+		//-----------------------------------------------------------
+		// Member methods.
+		//
+
+		//-----------------------------------------------------------
+		// Member data.
+		//
+
+		//-------------------------------------------------------
+		// Statics.
+		//
+		/* package */ static RedigitalizePolygon_Default.RedigitalizePolygon_FirstPoint FirstPoint;
+		/* package */ static RedigitalizePolygon_Default.RedigitalizePolygon_SecondPoint SecondPoint;
+		/* package */ static RedigitalizePolygon_Default.RedigitalizePolygon_NextPoint NextPoint;
+		private static RedigitalizePolygon_Default Default;
+
+		static
+		{
+			FirstPoint = new RedigitalizePolygon_Default.RedigitalizePolygon_FirstPoint("RedigitalizePolygon.FirstPoint", 0);
+			SecondPoint = new RedigitalizePolygon_Default.RedigitalizePolygon_SecondPoint("RedigitalizePolygon.SecondPoint", 1);
+			NextPoint = new RedigitalizePolygon_Default.RedigitalizePolygon_NextPoint("RedigitalizePolygon.NextPoint", 2);
+			Default = new RedigitalizePolygon_Default("RedigitalizePolygon.Default", -1);
+		}
+
+	}
+
+	protected static class RedigitalizePolygon_Default
+	extends RedigitalizePolygonCADToolState
+	{
+		//-----------------------------------------------------------
+		// Member methods.
+		//
+
+		protected RedigitalizePolygon_Default(String name, int id)
+		{
+			super (name, id);
+		}
+
+		protected void addPoint(RedigitalizePolygonCADToolContext context, double pointX, double pointY, InputEvent event)
+		{
+			RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+			boolean loopbackFlag =
+				context.getState().getName().equals(
+						RedigitalizePolygon.FirstPoint.getName());
+
+			if (loopbackFlag == false)
+			{
+				(context.getState()).Exit(context);
+			}
+
+			context.clearState();
+			try
+			{
+				ctxt.throwPointException(PluginServices.getText(this,"incorrect_point"), pointX, pointY);
+			}
+			finally
+			{
+				context.setState(RedigitalizePolygon.FirstPoint);
+
+				if (loopbackFlag == false)
+				{
+					(context.getState()).Entry(context);
+				}
+
+			}
+			return;
+		}
+
+		protected void removePoint(RedigitalizePolygonCADToolContext context, InputEvent event, int numPoints)
+		{
+			RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+			boolean loopbackFlag =
+				context.getState().getName().equals(
+						RedigitalizePolygon.FirstPoint.getName());
+
+			if (loopbackFlag == false)
+			{
+				(context.getState()).Exit(context);
+			}
+
+			context.clearState();
+			try
+			{
+				ctxt.throwNoPointsException(PluginServices.getText(this,"no_points"));
+			}
+			finally
+			{
+				context.setState(RedigitalizePolygon.FirstPoint);
+
+				if (loopbackFlag == false)
+				{
+					(context.getState()).Entry(context);
+				}
+
+			}
+			return;
+		}
+
+		protected void addOption(RedigitalizePolygonCADToolContext context, String s)
+		{
+			RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+			if (s.equals("C")||s.equals("c")||s.equals(PluginServices.getText(this,"cancel")))
+			{
+				boolean loopbackFlag =
+					context.getState().getName().equals(
+							RedigitalizePolygon.FirstPoint.getName());
+
+				if (loopbackFlag == false)
+				{
+					(context.getState()).Exit(context);
+				}
+
+				context.clearState();
+				try
+				{
+					ctxt.clear();
+				}
+				finally
+				{
+					context.setState(RedigitalizePolygon.FirstPoint);
+
+					if (loopbackFlag == false)
+					{
+						(context.getState()).Entry(context);
+					}
+
+				}
+			}
+			else
+			{
+				RedigitalizePolygonCADToolState endState = context.getState();
+
+				context.clearState();
+				try
+				{
+					ctxt.throwOptionException(PluginServices.getText(this,"incorrect_option"), s);
+				}
+				finally
+				{
+					context.setState(endState);
+				}
+			}
+
+			return;
+		}
+
+		//-----------------------------------------------------------
+		// Inner classse.
+		//
+
+
+		private static final class RedigitalizePolygon_FirstPoint
+		extends RedigitalizePolygon_Default
+		{
+			//-------------------------------------------------------
+			// Member methods.
+			//
+
+			private RedigitalizePolygon_FirstPoint(String name, int id)
+			{
+				super (name, id);
+			}
+
+			protected void Entry(RedigitalizePolygonCADToolContext context)
+			{
+				RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+				ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_insert_first_point"));
+				ctxt.setDescription(new String[]{"cancel"});
+				return;
+			}
+
+			protected void addPoint(RedigitalizePolygonCADToolContext context, double pointX, double pointY, InputEvent event)
+			{
+				RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+				if (ctxt.pointInsideFeature(pointX,pointY))
+				{
+
+					(context.getState()).Exit(context);
+					context.clearState();
+					try
+					{
+						ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_insert_second_point"));
+					}
+					finally
+					{
+						context.setState(RedigitalizePolygon.SecondPoint);
+						(context.getState()).Entry(context);
+					}
+				}
+				else
+				{
+					RedigitalizePolygonCADToolState endState = context.getState();
+
+					context.clearState();
+					try
+					{
+						ctxt.throwPointException(PluginServices.getText(this,"redigitaliza_incorrect_point"), pointX, pointY);
+						ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_insert_first_point"));
+					}
+					finally
+					{
+						context.setState(endState);
+					}
+				}
+
+				return;
+			}
+
+			//-------------------------------------------------------
+			// Member data.
+			//
+		}
+
+		private static final class RedigitalizePolygon_SecondPoint
+		extends RedigitalizePolygon_Default
+		{
+			//-------------------------------------------------------
+			// Member methods.
+			//
+
+			private RedigitalizePolygon_SecondPoint(String name, int id)
+			{
+				super (name, id);
+			}
+
+			protected void addPoint(RedigitalizePolygonCADToolContext context, double pointX, double pointY, InputEvent event)
+			{
+				RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+				if (ctxt.secondPointInsideFeature(pointX,pointY))
+				{
+
+					(context.getState()).Exit(context);
+					context.clearState();
+					try
+					{
+						ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_poligono_insert_other_point"));
+					}
+					finally
+					{
+						context.setState(RedigitalizePolygon.NextPoint);
+						(context.getState()).Entry(context);
+					}
+				}
+				else
+				{
+					RedigitalizePolygonCADToolState endState = context.getState();
+
+					context.clearState();
+					try
+					{
+						ctxt.throwPointException(PluginServices.getText(this,"redigitaliza_incorrect_point"), pointX, pointY);
+						ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_insert_second_point"));
+					}
+					finally
+					{
+						context.setState(endState);
+					}
+				}
+
+				return;
+			}
+
+			protected void removePoint(RedigitalizePolygonCADToolContext context, InputEvent event, int numPoints)
+			{
+				RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+
+				(context.getState()).Exit(context);
+				context.clearState();
+				try
+				{
+					ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_insert_first_point"));
+					ctxt.removeFirstPoint(event);
+				}
+				finally
+				{
+					context.setState(RedigitalizePolygon.FirstPoint);
+					(context.getState()).Entry(context);
+				}
+				return;
+			}
+
+			//-------------------------------------------------------
+			// Member data.
+			//
+		}
+
+		private static final class RedigitalizePolygon_NextPoint
+		extends RedigitalizePolygon_Default
+		{
+			//-------------------------------------------------------
+			// Member methods.
+			//
+
+			private RedigitalizePolygon_NextPoint(String name, int id)
+			{
+				super (name, id);
+			}
+
+			protected void addOption(RedigitalizePolygonCADToolContext context, String s)
+			{
+				RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+// NACHOV if ((((s.equals("g")||s.equals("G")))) && ctxt.checksOnEdition(ctxt.getGeometriaResultante(), ctxt.getCurrentGeoid())))
+				if (s.equals("espacio")) {
+
+					(context.getState()).Exit(context);
+					context.clearState();
+					try {
+						ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_insert_first_point"));
+						ctxt.saveChanges();
+						ctxt.clear();
+					}
+					finally {
+						context.setState(RedigitalizePolygon.FirstPoint);
+						(context.getState()).Entry(context);
+					}
+				}
+//				else if (s.equals("g")||s.equals("G"))
+//				{
+
+//				// No actions.
+//				}
+				else if (s.equals("tab")) {
+					
+					RedigitalizePolygonCADToolState endState = context.getState();
+					context.clearState();
+					
+					try {
+						ctxt.changesPolygonPart();
+					}
+					finally {
+						context.setState(endState);
+					}
+				} else {
+					super.addOption(context, s);
+				}
+				return;
+			}
+
+			protected void addPoint(RedigitalizePolygonCADToolContext context, double pointX, double pointY, InputEvent event)
+			{
+				RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+				RedigitalizePolygonCADToolState endState = context.getState();
+
+				context.clearState();
+				try
+				{
+					ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_poligono_insert_other_point"));
+					ctxt.addPoint(pointX, pointY, event);
+				}
+				finally
+				{
+					context.setState(endState);
+				}
+				return;
+			}
+
+			protected void removePoint(RedigitalizePolygonCADToolContext context, InputEvent event, int numPoints)
+			{
+				RedigitalizePolygonCADTool ctxt = context.getOwner();
+
+				if (numPoints>0)
+				{
+					RedigitalizePolygonCADToolState endState = context.getState();
+
+					context.clearState();
+					try
+					{
+						ctxt.removePoint(event);
+					}
+					finally
+					{
+						context.setState(endState);
+					}
+				}
+				else if (numPoints==0)
+				{
+
+					(context.getState()).Exit(context);
+					context.clearState();
+					try
+					{
+						ctxt.setQuestion(PluginServices.getText(this,"redigitaliza_insert_second_point"));
+						ctxt.removeSecondPoint(event);
+					}
+					finally
+					{
+						context.setState(RedigitalizePolygon.SecondPoint);
+						(context.getState()).Entry(context);
+					}
+				}                else
+				{
+					super.removePoint(context, event, numPoints);
+				}
+
+				return;
+			}
+
+			//-------------------------------------------------------
+			// Member data.
+			//
+		}
+
+		//-----------------------------------------------------------
+		// Member data.
+		//
+	}
+}
